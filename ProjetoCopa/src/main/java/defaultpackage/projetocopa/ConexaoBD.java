73f6e72d123d42c713a73617be7fbd1f2d2713a6
s1cilian <117952486+s1cilian@users.noreply.github.com>
2022-11-10 19:59:54 -0300

    Add files via upload

---
+++ ProjetoCopa/src/main/java/defaultpackage/projetocopa/ConexaoBD.java
@@ -0,0 +1,30 @@
+
+package defaultpackage.projetocopa;
+//COPA
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexaoBD {
+    private static String host = "localhost";
+    private static String porta = "3306";
+    private static String db = "projeto_copa";
+    private static String usuario = "root";
+    private static String senha = "goal123";
+    
+    public Connection obtemConexao(){
+        try{
+            String url = String.format(
+                "jdbc:mysql://%s:%s/%s?useTimezone=true&serverTimezone=UTC",
+                host, porta, db
+            );
+            Connection c = DriverManager.getConnection(url, usuario, senha);
+            return c; 
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+}
